d4188bbaf7659600e42a774f9cefcd6b35789a5a
Maria <mariafcknt@outlook.com>
2024-10-02 20:04:12 -0300

    WIP: construindo as entidades

--- microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/entity/Aluno.java
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/entity/Aluno.java
@@ -2,4 +2,14 @@ package br.univille.microservsecretaria.cadastroaluno.entity;
 
 public class Aluno {
     private String nome;
+    //private cursoGraduacao
+    private DiplomaEscola diploma;
+    private StatusAluno status;
+
+    public Aluno(String nome, DiplomaEscola diploma, StatusAluno status) {
+        this.nome = nome;
+        this.diploma = diploma;
+        this.status = status;
+    }
+
 }
---
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/entity/DiplomaEscola.java
@@ -0,0 +1,12 @@
+package br.univille.microservsecretaria.cadastroaluno.entity;
+
+public class DiplomaEscola {
+    private long matriculaEscola;
+    private String nomeEscola;
+
+
+    public DiplomaEscola(long matriculaEscola, String nomeEscola) {
+        this.matriculaEscola = matriculaEscola;
+        this.nomeEscola = nomeEscola;
+    }
+}
