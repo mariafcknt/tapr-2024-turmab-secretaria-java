48865f97bc86ec0dd08f9bbee09a3da96f267e7e
Guilherme Raiser <105473776+GuilhermeRaiser@users.noreply.github.com>
2024-10-02 22:14:36 +0000

    teste

--- microservsecretaria/src/main/java/br/univille/microservsecretaria/transferenciaaluno/entity/Transferencia.java
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/transferenciaaluno/entity/Transferencia.java
@@ -1,5 +1,7 @@
 package br.univille.microservsecretaria.transferenciaaluno.entity;
 
 public class Transferencia {
-    
+    public static void main(String[] args) {
+        System.out.println("testa branch e workspace");
+    }
 }
