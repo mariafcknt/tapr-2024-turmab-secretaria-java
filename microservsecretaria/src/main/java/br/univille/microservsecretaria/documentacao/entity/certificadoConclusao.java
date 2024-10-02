31a08df215a3c80d8e55f821fc5c94ee94e017ca
Guilherme Raiser <105473776+GuilhermeRaiser@users.noreply.github.com>
2024-10-02 23:01:40 +0000

    WIP: Iniciando a classe certificadoConclusao

---
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/documentacao/entity/certificadoConclusao.java
@@ -0,0 +1,14 @@
+package br.univille.microservsecretaria.documentacao.entity;
+
+public class certificadoConclusao {
+
+   UUID id;
+   Aluno matricula;
+   UUID desempenhoAcademico;
+
+ 
+   public void emitirCertificado (){
+      
+   }
+
+}
