275f83e9a5c1b471e5372ea0a005fc48b68d0c85
Maria <mariafcknt@outlook.com>
2024-11-06 00:32:06 +0000

    criacao config

--- microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/entity/Aluno.java
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/entity/Aluno.java
@@ -1,7 +1,5 @@
 package br.univille.microservsecretaria.cadastroaluno.entity;
 
-import java.util.UUID;
-
 import org.springframework.data.annotation.Id;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
