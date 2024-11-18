35689282224c7e511e60de6578f0cb22aaceb5fc
Maria <mariafcknt@outlook.com>
2024-11-18 01:30:30 +0000

    delete method api

--- microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/controller/AlunoAPIController.java
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/cadastroaluno/controller/AlunoAPIController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,4 +52,17 @@ public class AlunoAPIController {
         }
         return new ResponseEntity<Aluno>(aluno,HttpStatus.OK);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Aluno> delete(@PathVariable("id") String id){
+        if(id == "" || id == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        var aluno = service.delete(id);
+        if (aluno != null){
+            return new ResponseEntity<Aluno>(aluno,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
